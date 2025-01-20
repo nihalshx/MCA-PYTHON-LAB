9dcb0d0904458ec8144e65fd7b989e06c423bd99
Nihal N <112004307+nihalshx@users.noreply.github.com>
2025-01-20 08:06:22 +0530

    Delete NOV/27-11-24/math_operations directory

--- NOV/27-11-24/math_operations/geometry/area.py
+++
@@ -1,9 +0,0 @@
-def rectangle_area(len, width):
-    return len * width
-
-def circle_area(radius):
-    from math import pi
-    return pi * radius * radius
-
-def triangle_area(base, height):
-    return 0.5 * base * height
